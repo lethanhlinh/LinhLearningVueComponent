9cd35a035d1c9fd2b491339b43437a0a9b05db5a
LeThanhLinh <lenguyenthanhlinh9a1819@gmail.com>
2025-07-31 22:11:00 +0700

    create and use component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> Hello World </h1>
+  <FirstComponent></FirstComponent>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import FirstComponent from './components/FirstComponent.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FirstComponent
   }
 }
 </script>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>My First Component</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+h1 {
+  background-color: aqua;
+  color: darkblue;
+}
+</style>
